f60f14075a277ef4b0e6a816c87c5ac8e2e518e6
alvcontrerass <alvarocontrerasilva@gmail.com>
2024-08-13 12:45:23 -0400

    actualizacion gulp 5 & sass 1.77

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,32 @@
-const { src, dest, watch } = require('gulp')
+//Version gulp 4.0.0 y Node con tipeado commonJS
+/*const { src, dest, watch } = require('gulp')
 const sass = require("gulp-sass")(require('sass'))
-const plumber = require('gulp-plumber')
+const plumber = require('gulp-plumber')*/
+//css = done => {
+//    src('src/scss/**/*.scss').pipe(plumber()).pipe(sass()).pipe(dest("build/css"))
+//    done()
+//}
 
-css = cb => {
-    src('src/scss/**/*.scss').pipe(plumber()).pipe(sass()).pipe(dest("build/css"))
-    cb()
+//dev = done => {
+//    watch('src/scss/**/*.scss', css)
+//    done()
+//}
+
+//exports.dev = dev;
+
+//Version Gulp 5.0.0 y Node con tipeado Module
+import {src, dest, watch} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css(done) {
+    src('src/scss/**/*.scss').pipe(sass()).pipe(dest("build/css"))
+    done()
 }
 
-dev = cb => {
+export function dev() {
     watch('src/scss/**/*.scss', css)
-    cb()
 }
 
-exports.dev = dev;
